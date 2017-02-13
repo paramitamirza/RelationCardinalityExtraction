c85fff747283a031c0708b401fe7afc41b1d15ac
paramita <paramita@lap-15-61.mpi-inf.mpg.de>
2017-02-13 20:09:15 +0100

    Update src/

--- src/de/mpg/mpiinf/cardinality/autoextraction/SentenceExtractionFromWikipedia.java
+++ src/de/mpg/mpiinf/cardinality/autoextraction/SentenceExtractionFromWikipedia.java
@@ -2,21 +2,15 @@ package de.mpg.mpiinf.cardinality.autoextraction;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
 import org.json.JSONArray;
