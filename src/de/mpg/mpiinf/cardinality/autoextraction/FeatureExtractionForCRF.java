c8ef6e3dbe1300d29b7e550bfb6737d27aa6f0db
paramitamirza <paramita135@gmail.com>
2017-02-14 03:39:15 +0100

    Update src/

--- src/de/mpg/mpiinf/cardinality/autoextraction/FeatureExtractionForCRF.java
+++ src/de/mpg/mpiinf/cardinality/autoextraction/FeatureExtractionForCRF.java
@@ -11,10 +11,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.zip.GZIPInputStream;
 
 import org.json.*;
